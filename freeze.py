9a8e43b92fcef97833f3840a6b5e74d50e90158f
William D. Gizzi <gizzidan@me.com>
2017-01-30 11:47:56 -0500

    heroku now

--- freeze.py
+++
@@ -1,4 +0,0 @@
-import hello
-
-if __name__ == '__hello__':
-    hello.freezer.freeze()
--- hello.py
+++ hello.py
@@ -1,13 +1,11 @@
 from flask import Flask, url_for, render_template
 from flask_scss import Scss
-from flask_frozen import Freezer
 
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 Scss(app)
 Scss(app, static_dir='static', asset_dir='assets')
-freezer = Freezer(app)
 
 
 

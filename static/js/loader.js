4badaceb3cb03b4ee6d9f76688cee46cac0afec6
William D. Gizzi <gizzidan@me.com>
2017-01-30 13:51:35 -0500

    loader

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,4 +1,4 @@
-// Wait for window load
-$(window).on('load',function() {
-// Animate loader off screen
-$(".se-pre-con").fadeOut("slow");;
+$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+});

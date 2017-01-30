a480659e49598bb960c9a43eada64e6ab84be0be
William D. Gizzi <gizzidan@me.com>
2017-01-30 12:11:58 -0500

    heroku

--- hello.py
+++ hello.py
@@ -7,8 +7,10 @@ app.config.from_pyfile('settings.py')
 Scss(app)
 Scss(app, static_dir='static', asset_dir='assets')
 
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

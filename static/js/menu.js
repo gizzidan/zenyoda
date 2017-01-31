149f7485ba2c0fe8340995deedb4146005d40f05
William D. Gizzi <gizzidan@me.com>
2017-01-31 13:42:14 -0500

    minify

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,12 +1,1 @@
-
-;(function(){
-
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
-
-
-})(jQuery)
+!function(){$("#menuToggle, .menu-close").on("click",function(){$("#menuToggle").toggleClass("active"),$("body").toggleClass("body-push-toleft"),$("#theMenu").toggleClass("menu-open")})}(jQuery);
